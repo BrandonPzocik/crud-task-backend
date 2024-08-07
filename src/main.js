2a81417be5e65134b77e7d4c2fb55d4c114dd1da
Brandon <pzocikbrandon@gmail.com>
2024-08-07 11:50:59 -0300

    se termino el CRUD sin modularizar

--- src/main.js
+++ src/main.js
@@ -8,26 +8,44 @@ const {newConnection} = require("./bd")
 app.get("/tasks", async (req, res)=> {
     const connection = await newConnection()
     const result = await connection.query("SELECT * FROM tasks")
-    response.json(results[0])
+    res.json(results[0])
     connection.end()
 })
 
 //obtener una tarea por su id 
-app.get("/tasks/:id", async (request, response) => {
+app.get("/task/:id", async (req, res) => {
     const connection = await newConnection()
-    const id = request.params.id 
+    const {id}= req.params 
     const results = await connection.query("SELECT * FROM tasks WHERE id = ?", id)
-    response.json(results[0])
+    res.json(results[0])
     connection.end()
 }) 
 
 //añadir una nueva tarea 
 app.post("/tasks", async (req, res) => {
     const connection = await newConnection()
-    const {title, description, isComplete} = request.body 
-    connection.query("INSERT INTO tasks (title, description, isComplete)")
-    response.send("tarea creada correctamente")
+    const {title, description, isComplete} = req.body 
+    connection.query("INSERT INTO tasks (title, description, isComplete) values (?, ?, ?)", [title, description, isComplete])
+    res.send("tarea creada correctamente")
     connection.end()
+})
+// eliminar una tarea por id
+app.delete("/task/:id", async (req, res) => {
+    const connection = await newConnection()
+    const {id} = req.params
+    const results = await connection.query("DELETE FROM tasks WHERE id = ?", id)
+    res.json(results[0])
+    connection.end()
+})
+
+//editar una tarea por su id
+app.put("/task/:id", async (req, res) => {
+    const connection = await newConnection()
+    const {id} = req.params
+    const {title, description, isComplete} = req.body
+    await connection.query("UPDATE tasks SET title = ? , description = ?, isComplete = ? WHERE id = ?", [title, description, isComplete, id])
+    res.send("tarea actualizada con exito")
+    connection.end
 })
 
 app.listen(3000, () => {
